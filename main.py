61a5fc4024d1614910c6e5606b3da84a242e51d5
Nelson Malbone <nhmalbone@icloud.com>
2025-04-23 15:05:30 -0400

    04-23-2025 genertion category set up: thought i was doing it right last night but i didnt listen to my gut.

--- main.py
+++ main.py
@@ -7,5 +7,17 @@ current_year_input = int(input('Enter current year: '))
 # founding out how old the user is
 calc_yr_old = current_year_input - user_input
 
+# match case to check the user generation category
+match calc_yr_old:
+    case calc_yr_old if 60 <= calc_yr_old <= 77:
+        print("You fall into the generation of Baby Boomer")
+    case calc_yr_old if 44 <= calc_yr_old <= 59:
+        print("You Fall into the generation of Generation X")
+    case calc_yr_old if 28 <= calc_yr_old <= 43:
+        print("You fall into the generation of Millennials")
+    case calc_yr_old if 13 <= calc_yr_old <= 27:
+        print("You fall into the generation of Generation Z")
+    case calc_yr_old if 1 <= calc_yr_old <= 12:
+        print("You fall into the generation of Generation Alpha")
 # print Results
 print(f'You are {calc_yr_old} years of age')
